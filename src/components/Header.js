351d87e65648e47264c5ce9c1c0f1d37e90df180
Johannes Hageresch <johannes.hageresch@googlemail.com>
2021-04-27 12:38:03 +0200

    Create Header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <span className="header">Movie Entertaiment APP</span>
+    </div>
+  );
+};
+
+export default Header;
