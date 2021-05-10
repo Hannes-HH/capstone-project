8f753bcd494f7ea55a5b7ac67300005d3fadcfa4
Johannes Hageresch <johannes.hageresch@googlemail.com>
2021-05-10 12:25:19 +0200

    Fetching movie data by genre and build the API key.

---
+++ src/components/Genres/Genres.js
@@ -0,0 +1,63 @@
+import { Chip } from "@material-ui/core";
+import { useEffect } from "react";
+
+const Genres = ({
+  selectedGenres,
+  setSelectedGenres,
+  genres,
+  setGenres,
+  type,
+  setPage,
+}) => {
+  const handleAdd = (genre) => {
+    setSelectedGenres([...selectedGenres, genre]);
+    setGenres(genres.filter((g) => g.id !== genre.id));
+    setPage(1);
+  };
+
+  const handleRemove = (genre) => {
+    setSelectedGenres(
+      selectedGenres.filter((selected) => selected.id !== genre.id)
+    );
+    setGenres([...genres, genre]);
+    setPage(1);
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/genre/${type}/list?api_key=1e37d8deae00c9b519356f5d9412edbb&language=en-US`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setGenres(data.genres);
+      });
+  }, [setGenres, type]);
+
+  return (
+    <div style={{ padding: "6px 0" }}>
+      {selectedGenres.map((genre) => (
+        <Chip
+          style={{ margin: 2 }}
+          label={genre.name}
+          key={genre.id}
+          color="primary"
+          clickable
+          size="small"
+          onDelete={() => handleRemove(genre)}
+        />
+      ))}
+      {genres.map((genre) => (
+        <Chip
+          style={{ margin: 2 }}
+          label={genre.name}
+          key={genre.id}
+          clickable
+          size="small"
+          onClick={() => handleAdd(genre)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Genres;
